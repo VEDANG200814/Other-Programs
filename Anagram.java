6750723fda895265905171d70e9bdcdc709f3940
Vedang Chakraborty <91678229+VEDANG200814@users.noreply.github.com>
2024-11-07 00:22:53 +0530

    Prog

--- Anagram.java
+++ Anagram.java
@@ -5,15 +5,18 @@ public class Anagram {
         System.out.println("Enter two words");
         String w1=sc.next(),w2=sc.next();
         int f=1;
-        for(int i=0;i<w1.length();i++){
-            if(w2.indexOf(w1.charAt(i))==-1||w1.compareTo(w2)==0){
-                f=0;
-                break;
+        if(w1.length()==w2.length()){
+            for(int i=0;i<w1.length();i++){
+                if(w2.indexOf(w1.charAt(i))==-1||w1.compareTo(w2)==0){
+                    f=0;
+                    break;
+                }
             }
-        }
         if(f==1)
         System.out.println(w1+" and "+w2+" is an anagram");
         else
         System.out.println(w1+" and "+w2+" is not an anagram");
+        }
+        System.out.println("Wrong Input!!");
     }
 }
