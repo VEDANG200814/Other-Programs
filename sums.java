9a6006ac2594be8a93bae44cd72be66ee68a7eb9
Vedang Chakraborty <91678229+VEDANG200814@users.noreply.github.com>
2024-11-13 15:50:35 +0530

    Update sums.java

--- sums.java
+++ sums.java
@@ -3,15 +3,19 @@ public class sums {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number or enter 0 to exit");
-        int n=sc.nextInt(),sp=0,sn=0;
+        int n=sc.nextInt(),spo=0,spe=0,sn=0;
         while(n!=0){
-            if(n>0)
-            sp+=n;
+            if(n>0){
+                if(n%2==0)
+                spe+=n;
+                else
+                spo+=n;
+            }
             else
             sn+=n;
             System.out.println("Enter a number or enter 0 to exit");
             n=sc.nextInt();
         }
-        System.out.println("Sum of positive numbers: "+sp+"\nSum of negative numbers: "+sn);
+        System.out.println("Sum of positive odd numbers: "+spo+"\nSum of positive even numbers: "+spe+"\nSum of negative numbers: "+sn);
     }
 }
