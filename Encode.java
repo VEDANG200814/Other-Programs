0f0d1fa05dfd67edd4604be5ff1d3df7cd01a1a4
Vedang Chakraborty <91678229+VEDANG200814@users.noreply.github.com>
2024-11-18 23:54:35 +0530

    Prog

---
+++ Encode.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Encode{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string");
+        String s=sc.nextLine().toUpperCase(),s1="",v="AEIOU";
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(v.indexOf(ch)>=0)
+            s1+=++ch;
+            else
+            s1+=ch--;
+        }
+        System.out.println("New String: "+s1);
+    }
+}
