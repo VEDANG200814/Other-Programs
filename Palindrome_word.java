d94212b6ba9d0d8d4d80a8feb6984ac0a2b34a6a
Vedang Chakraborty <91678229+VEDANG200814@users.noreply.github.com>
2025-01-01 00:57:32 +0530

    Update Palindrome_word.java

--- Palindrome_word.java
+++ Palindrome_word.java
@@ -1,3 +1,20 @@
+import java.util.*;
 public class Palindrome_word {
-    
+    String palin(String s){
+        String s1="";
+        for(int i=s.length()-1;i>=0;i--){
+            s1=s1+s.charAt(i);
+        }
+        return s1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Palindrome_word ob = new Palindrome_word();
+        System.out.println("Enter a word: ");
+        String s=sc.next();
+        if(s.equalsIgnoreCase(ob.palin(s)))
+        System.out.println(s+" is a palindrome word");
+        else
+        System.out.println(s+" is not a palindrome word");
+    }
 }
